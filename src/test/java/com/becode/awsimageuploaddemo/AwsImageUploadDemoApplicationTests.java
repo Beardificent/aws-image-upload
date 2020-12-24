03cbe3ee1e7a4c983d109f828c543ab8dadfceec
Beardificent <cis.magito@gmail.com>
2020-12-24 11:05:16 +0100

    initial commit;

---
+++ src/test/java/com/becode/awsimageuploaddemo/AwsImageUploadDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.becode.awsimageuploaddemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AwsImageUploadDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
